0f0490b62d02ce7d04517ca9c91eb3d49ed2494f
maikynata <maikynata25@gmail.com>
2024-11-27 12:23:22 -0300

    new audit 2023

--- UPDATE-ALTER-table-itens-saida.sql
+++ UPDATE-ALTER-table-itens-saida.sql
@@ -3,18 +3,18 @@
 CREATE TABLE itensSAIDA as SELECT COD_ITEM FROM bd_auditoria.reg_0200 group by COD_ITEM;
 
 ALTER TABLE bd_auditoria_2023.itenssaida
-ADD COLUMN QTDnfe062023 DECIMAL(15.3),
-ADD COLUMN QTDnfe062023 DECIMAL(15.3),
-ADD COLUMN QTDnfe062023 DECIMAL(15.3),
-ADD COLUMN QTDnfe062023 DECIMAL(15.3),
-ADD COLUMN QTDnfe062023 DECIMAL(15.3),
-ADD COLUMN QTDnfe062023 DECIMAL(15.3),
-ADD COLUMN QTDnfe072023 DECIMAL(15.3),
-ADD COLUMN QTDnfe082023 DECIMAL(15.3),
-ADD COLUMN QTDnfe092023 DECIMAL(15.3),
-ADD COLUMN QTDnfe102023 DECIMAL(15.3),
-ADD COLUMN QTDnfe112023 DECIMAL(15.3),
-ADD COLUMN QTDnfe062023 DECIMAL(15.3),
+ADD COLUMN QTDnfe122023 DECIMAL(15.3),
+ADD COLUMN QTDnfe122023 DECIMAL(15.3),
+ADD COLUMN QTDnfe122023 DECIMAL(15.3),
+ADD COLUMN QTDnfe122023 DECIMAL(15.3),
+ADD COLUMN QTDnfe122023 DECIMAL(15.3),
+ADD COLUMN QTDnfe122023 DECIMAL(15.3),
+ADD COLUMN QTDnfe122023 DECIMAL(15.3),
+ADD COLUMN QTDnfe122023 DECIMAL(15.3),
+ADD COLUMN QTDnfe122023 DECIMAL(15.3),
+ADD COLUMN QTDnfe122023 DECIMAL(15.3),
+ADD COLUMN QTDnfe122023 DECIMAL(15.3),
+ADD COLUMN QTDnfe122023 DECIMAL(15.3),
 ADD COLUMN TotalQTDnfe DECIMAL(15.3);
 
 
@@ -22,13 +22,13 @@ SELECT * FROM bd_auditoria_2023.itenssaida;
 
 -- Rodar create index antes do update:
 CREATE INDEX idx_cod_item ON bd_auditoria_2023.itenssaida(COD_ITEM);
-CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2023.mvtosaida062023(nfeProc_NFe_infNFe_det_prod_cProd);
+CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2023.mvtosaida122023(nfeProc_NFe_infNFe_det_prod_cProd);
 
 
-UPDATE bd_auditoria_2023.itenssaida INNER JOIN bd_auditoria_2023.mvtosaida062023
-    ON bd_auditoria_2023.itenssaida.COD_ITEM = bd_auditoria_2023.mvtosaida062023.nfeProc_NFe_infNFe_det_prod_cProd
-SET itenssaida.QTDnfe062023 = mvtosaida062023.qtd;
+UPDATE bd_auditoria_2023.itenssaida INNER JOIN bd_auditoria_2023.mvtosaida122023
+    ON bd_auditoria_2023.itenssaida.COD_ITEM = bd_auditoria_2023.mvtosaida122023.nfeProc_NFe_infNFe_det_prod_cProd
+SET itenssaida.QTDnfe122023 = mvtosaida122023.qtd;
 
 UPDATE bd_auditoria_2023.itenssaida
-SET QTDnfe062023 = 0
-where QTDnfe062023 is NULL;
+SET QTDnfe122023 = 0
+where QTDnfe122023 is NULL;
