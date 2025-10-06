c2fccc715692caf032c5a5038c50c439fab2d2e0
maikynata <maikynata25@gmail.com>
2025-10-06 11:47:11 -0300

    2023 finalizada entregue

--- 2023/05.CREATE-table-mvtosaida.sql
+++ 2023/05.CREATE-table-mvtosaida.sql
@@ -27,6 +27,9 @@ FROM bd_auditoria_2023.nfe122023
 where nfeProc_NFe_infNFe_det_prod_cProd='407828' and nfeProc_NFe_infNFe_det_prod_CFOP in (5102,5152,5405,5409,5910,5911,5926,5949,6152,6409,6949,5202,5411,6202,6411,6108,5209,6209)
 order by nfeProc_NFe_infNFe_ide_nNF;
 
+SELECT * FROM bd_auditoria_2023.mvtosaida122023
+WHERE nfeProc_NFe_infNFe_det_prod_cProd=407828;
+
 SELECT nfeProc_NFe_infNFe_det_prod_cProd, nfeProc_NFe_infNFe_Id, nfeProc_NFe_infNFe_ide_nNF, nfeProc_NFe_infNFe_det_prod_qCom, nfeProc_NFe_infNFe_det_prod_CFOP 
 FROM bd_auditoria_2023.nfe122023
 where nfeProc_NFe_infNFe_det_prod_cProd='352659' and nfeProc_NFe_infNFe_det_prod_CFOP in (5152,6152,5409,6409)
