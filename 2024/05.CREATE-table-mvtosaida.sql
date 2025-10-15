e3f6f69b6b13822ffdac36b574486efbce1d12cb
maikynata <maikynata25@gmail.com>
2025-10-15 12:16:30 -0300

    finish 2024

--- 2024/05.CREATE-table-mvtosaida.sql
+++ 2024/05.CREATE-table-mvtosaida.sql
@@ -8,31 +8,31 @@ SELECT * FROM bd_auditoria_2024.nfe062024 WHERE nfeProc_NFe_infNFe_ide_dhEmi NOT
 SELECT * FROM bd_auditoria_2024.nfe072024 WHERE nfeProc_NFe_infNFe_ide_dhEmi NOT LIKE '2024-07-%';
 SELECT * FROM bd_auditoria_2024.nfe082024 WHERE nfeProc_NFe_infNFe_ide_dhEmi NOT LIKE '2024-08-%';
 SELECT * FROM bd_auditoria_2024.nfe092024 WHERE nfeProc_NFe_infNFe_ide_dhEmi NOT LIKE '2024-09-%';
-SELECT * FROM bd_auditoria_2024.nfe102024 WHERE nfeProc_NFe_infNFe_ide_dhEmi NOT LIKE '2024-10-%';
+SELECT * FROM bd_auditoria_2024.nfe032024 WHERE nfeProc_NFe_infNFe_ide_dhEmi NOT LIKE '2024-10-%';
 SELECT * FROM bd_auditoria_2024.nfe112024 WHERE nfeProc_NFe_infNFe_ide_dhEmi NOT LIKE '2024-11-%';
-SELECT * FROM bd_auditoria_2024.nfe102024 WHERE nfeProc_NFe_infNFe_ide_dhEmi NOT LIKE '2024-12-%';
+SELECT * FROM bd_auditoria_2024.nfe032024 WHERE nfeProc_NFe_infNFe_ide_dhEmi NOT LIKE '2024-12-%';
 
 
-CREATE TABLE mvtoSAIDA102024 as (SELECT nfeProc_NFe_infNFe_det_prod_cProd, SUM(nfeProc_NFe_infNFe_det_prod_qCom) as qtd, nfeProc_NFe_infNFe_det_prod_CFOP 
-FROM bd_auditoria_2024.nfe102024
+CREATE TABLE mvtoSAIDA032024 as (SELECT nfeProc_NFe_infNFe_det_prod_cProd, SUM(nfeProc_NFe_infNFe_det_prod_qCom) as qtd, nfeProc_NFe_infNFe_det_prod_CFOP 
+FROM bd_auditoria_2024.nfe032024
 where nfeProc_NFe_infNFe_det_prod_CFOP in (5102,5152,5405,5409,5910,5911,5926,5949,6152,6409,6949,5202,5411,6202,6411,6108,5209,6209)
 group by nfeProc_NFe_infNFe_det_prod_cProd);
 
 -- CREATE somente sem notas de Mudança de endereço:
--- CREATE TABLE mvtoSAIDA102024 as (SELECT nfeProc_NFe_infNFe_det_prod_cProd, SUM(nfeProc_NFe_infNFe_det_prod_qCom) as qtd, nfeProc_NFe_infNFe_det_prod_CFOP 
--- FROM bd_auditoria_2024.nfe102024
+-- CREATE TABLE mvtoSAIDA032024 as (SELECT nfeProc_NFe_infNFe_det_prod_cProd, SUM(nfeProc_NFe_infNFe_det_prod_qCom) as qtd, nfeProc_NFe_infNFe_det_prod_CFOP 
+-- FROM bd_auditoria_2024.nfe032024
 -- where nfeProc_NFe_infNFe_det_prod_CFOP in (5102,5152,5405,5409,5910,5911,5926,5949,6152,6409,6949,5202,5411,6202,6411,6108,5209,6209)
 -- and nfeProc_NFe_infNFe_ide_natOp <> "Mudan. de End. - Trans. de estoque de m" 
 -- group by nfeProc_NFe_infNFe_det_prod_cProd);
 
-SELECT * FROM mvtoSAIDA102024;
+SELECT * FROM mvtoSAIDA032024;
 
-SELECT nfeProc_NFe_infNFe_det_prod_cProd FROM mvtoSAIDA102024
+SELECT nfeProc_NFe_infNFe_det_prod_cProd FROM mvtoSAIDA032024
 where nfeProc_NFe_infNFe_det_prod_cProd not in (SELECT COD_ITEM FROM bd_auditoria_2024.itenssaida);
 
--- SELECT * FROM bd_auditoria_2024.nfe102024 limit 10;
+-- SELECT * FROM bd_auditoria_2024.nfe032024 limit 10;
 
--- SELECT SUM(nfeProc_NFe_infNFe_det_prod_qCom) FROM bd_auditoria_2024.nfe102024
+-- SELECT SUM(nfeProc_NFe_infNFe_det_prod_qCom) FROM bd_auditoria_2024.nfe032024
 -- where nfeProc_NFe_infNFe_det_prod_CFOP in (5102,5152,5405,5409,5910,5911,5926,5949,6152,6409,6949,5202,5411,6202,6411) group by nfeProc_NFe_infNFe_det_prod_cProd;
 
 SELECT nfeProc_NFe_infNFe_det_prod_cProd, nfeProc_NFe_infNFe_Id, nfeProc_NFe_infNFe_ide_nNF, nfeProc_NFe_infNFe_det_prod_qCom, nfeProc_NFe_infNFe_det_prod_CFOP, nfeProc_NFe_infNFe_ide_mod 
@@ -41,11 +41,11 @@ where nfeProc_NFe_infNFe_det_prod_cProd='541664' and nfeProc_NFe_infNFe_det_prod
 order by nfeProc_NFe_infNFe_ide_nNF;
 
 SELECT nfeProc_NFe_infNFe_det_prod_cProd, nfeProc_NFe_infNFe_Id, nfeProc_NFe_infNFe_ide_nNF, nfeProc_NFe_infNFe_det_prod_qCom, nfeProc_NFe_infNFe_det_prod_CFOP 
-FROM bd_auditoria_2024.nfe102024
+FROM bd_auditoria_2024.nfe032024
 where nfeProc_NFe_infNFe_det_prod_cProd='352659' and nfeProc_NFe_infNFe_det_prod_CFOP in (5152,6152,5409,6409)
 order by nfeProc_NFe_infNFe_ide_nNF;
 
-SELECT nfeProc_NFe_infNFe_det_prod_CFOP FROM bd_auditoria_2024.nfe102024
+SELECT nfeProc_NFe_infNFe_det_prod_CFOP FROM bd_auditoria_2024.nfe032024
 where nfeProc_NFe_infNFe_ide_mod='55'
 group by nfeProc_NFe_infNFe_det_prod_CFOP
 order by nfeProc_NFe_infNFe_det_prod_CFOP;
--- 2024/06.UPDATE-ALTER-table-itens-saida.sql
+++ 2024/06.UPDATE-ALTER-table-itens-saida.sql
@@ -9,18 +9,18 @@ FROM bd_auditoria_2024.reg_h010;
 
 
 ALTER TABLE bd_auditoria_2024.itenssaida
-ADD COLUMN QTDnfe062024 DECIMAL(15.3),
-ADD COLUMN QTDnfe062024 DECIMAL(15.3),
-ADD COLUMN QTDnfe062024 DECIMAL(15.3),
-ADD COLUMN QTDnfe062024 DECIMAL(15.3),
-ADD COLUMN QTDnfe062024 DECIMAL(15.3),
-ADD COLUMN QTDnfe062024 DECIMAL(15.3),
-ADD COLUMN QTDnfe072024 DECIMAL(15.3),
-ADD COLUMN QTDnfe082024 DECIMAL(15.3),
-ADD COLUMN QTDnfe092024 DECIMAL(15.3),
-ADD COLUMN QTDnfe102024 DECIMAL(15.3),
-ADD COLUMN QTDnfe112024 DECIMAL(15.3),
-ADD COLUMN QTDnfe122024 DECIMAL(15.3),
+ADD COLUMN QTDnfe032024 DECIMAL(15.3),
+ADD COLUMN QTDnfe032024 DECIMAL(15.3),
+ADD COLUMN QTDnfe032024 DECIMAL(15.3),
+ADD COLUMN QTDnfe032024 DECIMAL(15.3),
+ADD COLUMN QTDnfe032024 DECIMAL(15.3),
+ADD COLUMN QTDnfe032024 DECIMAL(15.3),
+ADD COLUMN QTDnfe032024 DECIMAL(15.3),
+ADD COLUMN QTDnfe032024 DECIMAL(15.3),
+ADD COLUMN QTDnfe032024 DECIMAL(15.3),
+ADD COLUMN QTDnfe032024 DECIMAL(15.3),
+ADD COLUMN QTDnfe032024 DECIMAL(15.3),
+ADD COLUMN QTDnfe032024 DECIMAL(15.3),
 ADD COLUMN TotalQTDnfe DECIMAL(15.3);
 
 
@@ -28,23 +28,23 @@ SELECT * FROM bd_auditoria_2024.itenssaida;
 
 -- Rodar create index antes do update:
 CREATE INDEX idx_cod_item ON bd_auditoria_2024.itenssaida(COD_ITEM);
-CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida062024(nfeProc_NFe_infNFe_det_prod_cProd);
-CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida062024(nfeProc_NFe_infNFe_det_prod_cProd);
-CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida062024(nfeProc_NFe_infNFe_det_prod_cProd);
-CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida062024(nfeProc_NFe_infNFe_det_prod_cProd);
-CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida062024(nfeProc_NFe_infNFe_det_prod_cProd);
-CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida062024(nfeProc_NFe_infNFe_det_prod_cProd);
-CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida062024(nfeProc_NFe_infNFe_det_prod_cProd);
-CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida062024(nfeProc_NFe_infNFe_det_prod_cProd);
-CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida062024(nfeProc_NFe_infNFe_det_prod_cProd);
-CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida102024(nfeProc_NFe_infNFe_det_prod_cProd);
-CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida062024(nfeProc_NFe_infNFe_det_prod_cProd);
-CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida062024(nfeProc_NFe_infNFe_det_prod_cProd);
+CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida032024(nfeProc_NFe_infNFe_det_prod_cProd);
+CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida032024(nfeProc_NFe_infNFe_det_prod_cProd);
+CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida032024(nfeProc_NFe_infNFe_det_prod_cProd);
+CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida032024(nfeProc_NFe_infNFe_det_prod_cProd);
+CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida032024(nfeProc_NFe_infNFe_det_prod_cProd);
+CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida032024(nfeProc_NFe_infNFe_det_prod_cProd);
+CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida032024(nfeProc_NFe_infNFe_det_prod_cProd);
+CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida032024(nfeProc_NFe_infNFe_det_prod_cProd);
+CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida032024(nfeProc_NFe_infNFe_det_prod_cProd);
+CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida032024(nfeProc_NFe_infNFe_det_prod_cProd);
+CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida032024(nfeProc_NFe_infNFe_det_prod_cProd);
+CREATE INDEX idx_nfeProc_NFe_infNFe_det_prod_cProd ON bd_auditoria_2024.mvtosaida032024(nfeProc_NFe_infNFe_det_prod_cProd);
 
 
-UPDATE bd_auditoria_2024.itenssaida INNER JOIN bd_auditoria_2024.mvtosaida062024
-    ON bd_auditoria_2024.itenssaida.COD_ITEM = bd_auditoria_2024.mvtosaida062024.nfeProc_NFe_infNFe_det_prod_cProd
-SET itenssaida.QTDnfe062024 = mvtosaida062024.qtd;
+UPDATE bd_auditoria_2024.itenssaida INNER JOIN bd_auditoria_2024.mvtosaida032024
+    ON bd_auditoria_2024.itenssaida.COD_ITEM = bd_auditoria_2024.mvtosaida032024.nfeProc_NFe_infNFe_det_prod_cProd
+SET itenssaida.QTDnfe032024 = mvtosaida032024.qtd;
 
 -- Corrigir para zero a coluna de saida quando o sql não encontra o produto no mvto de saida do mes (depois roda de novo o updade acima)
 -- Dessa forma o sql não irá alterar as qtds que estão zeradas, ou seja não teve mvto naquele mes
@@ -52,20 +52,20 @@ SELECT * FROM bd_auditoria_2024.itenssaida
 WHERE COD_ITEM=799267;
 
 SELECT * FROM bd_auditoria_2024.itenssaida
-WHERE COD_ITEM=171212;
+WHERE COD_ITEM=541664;
 
 UPDATE bd_auditoria_2024.itenssaida
-SET QTDnfe062024 = 0;
+SET QTDnfe032024 = 0;
 
 UPDATE bd_auditoria_2024.itenssaida
 SET TotalQTDnfe = 0;
 
 UPDATE bd_auditoria_2024.itenssaida
-SET QTDnfe062024 = 0
-where QTDnfe062024 is NULL;
+SET QTDnfe032024 = 0
+where QTDnfe032024 is NULL;
 
 -- Update total das saidas após corrigir o mes a mes:
 
 UPDATE bd_auditoria_2024.itenssaida
-SET TotalQTDnfe = (QTDnfe062024 + QTDnfe062024 + QTDnfe062024 + QTDnfe062024 + QTDnfe062024 + QTDnfe062024 + QTDnfe062024 + QTDnfe062024 + QTDnfe062024
-+ QTDnfe102024 + QTDnfe112024 + QTDnfe062024);
+SET TotalQTDnfe = (QTDnfe012024 + QTDnfe022024 + QTDnfe032024 + QTDnfe042024 + QTDnfe052024 + QTDnfe062024 + QTDnfe072024 + QTDnfe082024 + QTDnfe092024
++ QTDnfe102024 + QTDnfe112024 + QTDnfe122024);
